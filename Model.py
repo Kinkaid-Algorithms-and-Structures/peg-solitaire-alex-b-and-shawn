8191767444fa4c41bfb22face08293fc00c88b4f
shaandhutia <shaan.dhutia@kinkaid.org>
2024-08-30 09:18:47 -0500

    Added Model Class

---
+++ Model.py
@@ -0,0 +1,15 @@
+class Model:
+    def __init__(self):
+        self.num_pegs_left = 0
+        self.game_board = [[5], 5]
+        print(self.game_board)
+
+    def fill_board(self):
+        for(int i = )
+
+    def move_peg(self, peg, final_position):
+        pass
+
+if __name__ == "__main__":
+    Game = Model()
+
