caa42ada50d1e7d6214f5c2770edafcef1aa245b
shaandhutia <shaan.dhutia@kinkaid.org>
2024-08-30 09:46:56 -0500

    added fill_board() method

--- Model.py
+++ Model.py
@@ -1,14 +1,17 @@
 class Model:
     def __init__(self):
         self.num_pegs_left = 0
-        self.game_board = [[5], 5]
+        self.game_board = [[0 for c in range(5)] for r in range(5)]
+        self.fill_board()
         print(self.game_board)
 
     def fill_board(self):
-        for(int i = )
+        for r in range(0, 5):
+            for c in range(0, r+1):
+                self.game_board[r][c] = 1
 
     def move_peg(self, peg, final_position):
-        pass
+        if()
 
 if __name__ == "__main__":
     Game = Model()
