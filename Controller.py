e6b5e9a9b58e260d865d0b1e17fdbc64d22e5362
alexanderb <alex.boylan@kinkaid.org>
2024-09-04 09:49:57 -0500

    Final push of day 2.

--- Controller.py
+++ Controller.py
@@ -1,13 +1,40 @@
+from itertools import filterfalse
+
 from Model import Model
 class Controller:
     def __init__(self):
         self.any_valid_moves = False
         self.has_won = False
         self.starting_hole = [0,0]
+        self.MyModel = Model()
 
-    def is_move_valid(self, peg, final_position):
-        if Model.game_board[final_position] is not None:
+    def is_in_bounds(self, position_X, position_Y):
+        """
+
+        :param position_X:
+        :param position_Y:
+        :return: boolean
+        """
+        if position_X >= 5:
             return False
+        elif position_Y >= 5:
+            return False
+        else:
+            return True
+
+    def is_move_valid(self, final_positionX, final_positionY, initial_positionX, initial_positionY):
+        if self.is_in_bounds(final_positionX, final_positionY) == False or self.is_in_bounds(initial_positionX, initial_positionY) == False:
+            return False
+        if self.MyModel.game_board[final_positionY][final_positionX] is not None:
+            return False
+        elif self.MyModel.game_board[final_positionY][final_positionX] == self.MyModel.game_board[initial_positionY][initial_positionX]:
+            return False
+        if final_positionY == initial_positionY:
+            if initial_positionX == final_positionX + 1 or initial_positionX == final_positionX-1:
+                return False
+        if final_positionX == final_positionX:
+            if initial_positionY == final_positionY+1 or initial_positionY == final_positionY-1:
+                return False
 
 
 
--- Viewer.py
+++ Viewer.py
@@ -3,8 +3,9 @@ class Viewer:
     def __init__(self):
         self.peg_symbol = '📍'
         self.hole_symbol = '🕳'
+        self.MyModel = Model()
 
     def print_board(self):
-        for i in range(0, 4):
+        for i in range(0, 5):
             for j in range(0, i+1):
-                print(Model.game_board[i][j])
+                print(self.MyModel.game_board[i][j])
