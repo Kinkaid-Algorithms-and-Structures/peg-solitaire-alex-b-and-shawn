5f603679e36a27c85ce42292f26497b4731a63fc
shaandhutia <shaan.dhutia@kinkaid.org>
2024-09-06 08:53:21 -0500

    added check_valid_move() and is_inbounds() methods

--- Model.py
+++ Model.py
@@ -1,3 +1,6 @@
+from typing import final
+
+
 class Model:
     def __init__(self):
         self.num_pegs_left = 0
@@ -10,8 +13,22 @@ class Model:
             for c in range(0, r+1):
                 self.game_board[r][c] = 1
 
-    def move_peg(self, peg, final_position):
-        if()
+    def is_inbounds(self, row, col):
+        if row > 4 or col > 4 or row < 0 or col < 0:
+            return False
+
+        elif col > row:
+            return False
+
+        else:
+            return True
+
+    def check_valid_move(self, peg_row, peg_column, final_row, final_col):
+        if self.is_inbounds(final_row, final_col):
+            print("inbounds")
+        else: print ("outofbounds")
+
+
 
 if __name__ == "__main__":
     Game = Model()
